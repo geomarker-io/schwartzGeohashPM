435929b7ed652a853e94c68f280b01980dd91281
Erika <erika.rasnick@gmail.com>
2021-05-17 15:15:57 -0400

    test missing coords and out of order lat/lon columns

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -47,3 +47,46 @@ example_output_missing_sitecode <- function() {
   ) %>%
     dplyr::mutate_at(dplyr::vars(start_date, end_date, date), as.Date)
 }
+
+example_input_missing_coords <- function() {
+  tibble::tribble(
+    ~id,         ~lat,    ~lon, ~site_index,      ~sitecode,  ~start_date,    ~end_date,
+    '55000100280', NA, NA,   NA, NA, '2008-09-09', '2008-09-11',
+    '55000100282', 39.2, -84.6,   '9607238', 211050640897, '2015-08-31', '2015-09-02') %>%
+    dplyr::mutate_at(dplyr::vars(start_date, end_date), as.Date)
+}
+
+example_output_missing_coords <- function() {
+  tibble::tribble(
+    ~id, ~lat,  ~lon, ~site_index,    ~sitecode,  ~start_date,    ~end_date,        ~date,     ~gh6,  ~gh3, ~year, ~gh3_combined, ~PM25, ~NO2,  ~O3,
+    "55000100280", NA, NA,   NA, NA, "2008-09-09", "2008-09-11", "2008-09-09", NA, NA,  NA,         NA,   NA, NA, NA,
+    "55000100280", NA, NA,   NA, NA, "2008-09-09", "2008-09-11", "2008-09-10", NA, NA,  NA,         NA,   NA, NA, NA,
+    "55000100280", NA, NA,   NA, NA, "2008-09-09", "2008-09-11", "2008-09-11", NA, NA,  NA,         NA,   NA, NA, NA,
+    "55000100282", 39.2, -84.6,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-08-31", "dngz52", "dng",  2015,         "dng",  12.3, 32.7, 33.6,
+    "55000100282", 39.2, -84.6,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-09-01", "dngz52", "dng",  2015,         "dng",  17.9, 34.8,   44,
+    "55000100282", 39.2, -84.6,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-09-02", "dngz52", "dng",  2015,         "dng",  22.8, 34.2, 48.6
+  ) %>%
+    dplyr::mutate_at(dplyr::vars(start_date, end_date, date), as.Date)
+}
+
+
+example_input_out_of_order_cols <- function() {
+  tibble::tribble(
+    ~id,         ~lon,    ~lat, ~site_index,      ~sitecode,  ~start_date,    ~end_date,
+    '55000100280', -84.6, 39.2,   "9607238", 211050640897, '2008-09-09', '2008-09-11',
+    '55000100282', -84.6, 39.2,   '9607238', 211050640897, '2015-08-31', '2015-09-02') %>%
+    dplyr::mutate_at(dplyr::vars(start_date, end_date), as.Date)
+}
+
+example_output_out_of_order_cols <- function() {
+  tibble::tribble(
+    ~id, ~lon,  ~lat, ~site_index,    ~sitecode,  ~start_date,    ~end_date,        ~date,     ~gh6,  ~gh3, ~year, ~gh3_combined, ~PM25, ~NO2,  ~O3,
+    "55000100280", -84.6, 39.2,    "9607238", 211050640897, "2008-09-09", "2008-09-11", "2008-09-09", "dngz52", "dng",  2008,         "dng",   8.4, 27.7, 22.2,
+    "55000100280", -84.6, 39.2,   "9607238", 211050640897, "2008-09-09", "2008-09-11", "2008-09-10", "dngz52", "dng",  2008,         "dng",  10.2, 22.8, 21.2,
+    "55000100280", -84.6, 39.2,   "9607238", 211050640897, "2008-09-09", "2008-09-11", "2008-09-11", "dngz52", "dng",  2008,         "dng",  15.9, 27.8, 21.8,
+    "55000100282", -84.6, 39.2,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-08-31", "dngz52", "dng",  2015,         "dng",  12.3, 32.7, 33.6,
+    "55000100282", -84.6, 39.2,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-09-01", "dngz52", "dng",  2015,         "dng",  17.9, 34.8,   44,
+    "55000100282", -84.6, 39.2,   "9607238", 211050640897, "2015-08-31", "2015-09-02", "2015-09-02", "dngz52", "dng",  2015,         "dng",  22.8, 34.2, 48.6
+  ) %>%
+    dplyr::mutate_at(dplyr::vars(start_date, end_date, date), as.Date)
+}
--- tests/testthat/test-add_pollutants.R
+++ tests/testthat/test-add_pollutants.R
@@ -26,3 +26,30 @@ test_that("add_pollutants downloads chunks and joins to data with missing siteco
   delete_test_download_folder()
 })
 
+test_that("add_pollutants downloads chunks and joins to data with missing coordinates", {
+  delete_test_download_folder()
+  withr::with_envvar(new = c(
+    "AWS_ACCESS_KEY_ID" = NA,
+    "AWS_SECRET_ACCESS_KEY" = NA
+  ), {
+    expect_identical(
+      add_schwartz_pollutants(example_input_missing_coords(), confirm = FALSE),
+      example_output_missing_coords()
+    )
+  })
+  delete_test_download_folder()
+})
+
+test_that("add_pollutants downloads chunks and joins to data with out of order columns", {
+  delete_test_download_folder()
+  withr::with_envvar(new = c(
+    "AWS_ACCESS_KEY_ID" = NA,
+    "AWS_SECRET_ACCESS_KEY" = NA
+  ), {
+    expect_identical(
+      add_schwartz_pollutants(example_input_out_of_order_cols(), confirm = FALSE),
+      example_output_out_of_order_cols()
+    )
+  })
+  delete_test_download_folder()
+})
