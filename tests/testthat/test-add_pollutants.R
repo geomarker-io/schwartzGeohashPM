a23279e0e283fed8c1d16779f9a710ae61db6aed
Erika Rasnick <45949235+erikarasnick@users.noreply.github.com>
2021-06-30 09:37:32 -0400

    Out of range dates (#17)
    * filter missing dates before download, then add back after
    
    * out of range date test with correct input/output
    
    * check index date formatting

--- tests/testthat/test-add_pollutants.R
+++ tests/testthat/test-add_pollutants.R
@@ -33,7 +33,7 @@ test_that("add_pollutants downloads chunks and joins to data with missing coordi
     "AWS_SECRET_ACCESS_KEY" = NA
   ), {
     expect_identical(
-      add_schwartz_pollutants(example_input_missing_coords(), confirm = FALSE),
+      add_schwartz_pollutants(example_input_missing_coords(), confirm = FALSE), ### lat lon become not missing??
       example_output_missing_coords()
     )
   })
@@ -49,6 +49,23 @@ test_that("add_pollutants downloads chunks and joins to data with out of order c
     expect_identical(
       add_schwartz_pollutants(example_input_out_of_order_cols(), confirm = FALSE),
       example_output_out_of_order_cols()
+    )
+  })
+  delete_test_download_folder()
+})
+
+test_that("add_pollutants downloads chunks and joins to data with out of range dates", {
+  delete_test_download_folder()
+  d <- example_input()
+  d$start_date[1] <- "2020-01-01"
+  d$end_date[1] <- "2020-01-02"
+  withr::with_envvar(new = c(
+    "AWS_ACCESS_KEY_ID" = NA,
+    "AWS_SECRET_ACCESS_KEY" = NA
+  ), {
+    expect_identical(
+      add_schwartz_pollutants(d, confirm = FALSE),
+      example_output_out_of_range_dates()
     )
   })
   delete_test_download_folder()
